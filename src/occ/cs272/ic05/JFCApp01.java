bb1b26fb4e91b8c86157c3702dac49bb7245db6f
tpitts2 <tpitts2@O074CLASS-232V.COAST.CCCD.ADX>
2016-02-29 19:27:04 -0800

    efgegwegeg

--- src/occ/cs272/ic05/JFCApp01.java
+++ src/occ/cs272/ic05/JFCApp01.java
@@ -1,19 +1,25 @@
 package occ.cs272.ic05;
 
+import java.awt.EventQueue;
+
 import javax.swing.*;
 
 public class JFCApp01
 {
 
     public static void main(String args[]) {
-        
-        JFrame frame = new JFrame("This is the simplest JFC app");
-        
-        frame.setSize(600, 500);
-    
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        frame.setVisible(true);
+
+        EventQueue.invokeLater( new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("This is the simplest JFC app");
+
+                frame.setSize(600, 500);
+
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                frame.setVisible(true);
+            }   
+        });
     }
-    
+
 }
